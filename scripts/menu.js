3aeed0d532fdb3162d5c550cb7a573303738eaee
Marlon Silveira Gomes <marlonsilveiragomes@gmail.com>
2024-11-03 17:04:38 -0300

    Atualização do container apresentacaoCall

---
+++ scripts/menu.js
@@ -0,0 +1,4 @@
+document.getElementById('menuButton').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active'); // Alterna a classe active
+});
